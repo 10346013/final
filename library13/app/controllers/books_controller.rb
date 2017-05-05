3936ae31debbc6c738716ea4355e2c6a9740da2a
10346013 <10346013@ntub.edu.tw>
2017-05-04 22:27:32 -0400

    finish add book feature

--- library13/app/controllers/books_controller.rb
+++ library13/app/controllers/books_controller.rb
@@ -8,4 +8,38 @@ class BooksController < ApplicationController
       @book = Book.new
     end
   
+  def create
+      @book = Book.new(book_params)
+
+      if @book.save
+        redirect_to books_path, notice: "成功新增書本~~~Congratulations!"
+      else
+        render :new
+      end
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  private
+    def book_params
+      params.require(:book).permit(:bookname, :author, :price, :introduction)
+    end
+  
 end
