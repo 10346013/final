d21b42e735fab4f07b933c498a86bd0939456052
10346013 <10346013@ntub.edu.tw>
2017-06-16 01:09:54 -0400

    step-2

--- final-exam/app/models/score_paper.rb
+++ final-exam/app/models/score_paper.rb
@@ -1,2 +1,21 @@
 class ScorePaper < ApplicationRecord
+  def is_level(score_judge)   
+    
+    if score_judge >=90
+      return "S級"
+      else if score_judge <=89 && score_judge >=80
+        return "A級"
+        else if score_judge <=79 && score_judge >=70
+          return "B級"
+          else if score_judge <=69 && score_judge >=60
+            return "C級"
+            else if score_judge <60
+              return "D級"
+            end
+          end
+         end
+      end
+    end
+  end
+
 end
